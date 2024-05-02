d53654381709a920a23b6ce7de9448f97a4d792a
jyj051811@gmail.com <jyj051811@gmail.com>
2024-05-02 21:42:48 +0900

    데이터 정제

--- python/credit_data.py
+++ python/credit_data.py
@@ -2,36 +2,44 @@ import pandas as pd
 import statsmodels.api as sm
 
 # 데이터 파일 읽기
-df = pd.read_csv("credit_data.csv", low_memory=False)
+df = pd.read_csv("credit_data.csv", low_memory=False, na_values='*')
 
 # 원핫 인코딩 수행
-one_hot_encoded = pd.get_dummies(df['LIV_ADD'], prefix='LIV_ADD')
-print(one_hot_encoded)
-# print(one_hot_encoded)
-
-# # T OR F 형태의 데이터 수정
-# one_hot_encoded_replace = one_hot_encoded.replace({'True': 1, 'False': 0})
-# print(one_hot_encoded_replace)
+dummy = pd.get_dummies(df['LIV_ADD'], prefix='LIV_ADD', dtype=float)
+print(dummy.dtypes)
 
 # # 두 데이터프레임을 열 방향으로 합치기
-# combined_df = pd.concat([df, one_hot_encoded], axis=1)
-#
-# print(combined_df.dtypes)
-#
-# # 종속 변수 선택
-# y = combined_df["CB"]
-#
-# # 독립 변수 선택 (Y를 제외한 모든 변수)
-# X = combined_df.drop(columns=["CB", "SP", "LIV_ADD","RES_ADD","ADD_YN"])
-#
-# # 모든 독립 변수에 상수항 추가
-# X = sm.add_constant(X)
-#
-# # 모델 생성
-# model = sm.OLS(y, X)
-#
-# # 모델 피팅
-# result = model.fit()
-#
-# # AIC 값 확인
-# aic = result.aic
+combined_df = pd.concat([df, dummy], axis=1)
+non_numeric_columns = combined_df.select_dtypes(exclude=['number']).columns
+
+print("Non-numeric columns:", non_numeric_columns)
+
+
+# 종속 변수 선택
+y = combined_df["CB"]
+
+# 독립 변수 선택 (Y를 제외한 모든 변수)
+X = combined_df.drop(columns=["CB", "SP", "LIV_ADD","RES_ADD","ADD_YN"])
+
+# 모든 독립 변수에 상수항 추가
+X = sm.add_constant(X)
+
+print("Data types of y:", y.dtype)
+print("Data types of X:", X.dtypes)
+print("y values:", y.head())
+print("X values:", X.head())
+print("X type:", X.dtypes)
+print("X head:", X.head())
+
+
+
+
+
+# 모델 생성
+model = sm.OLS(y, X)
+
+# 모델 피팅
+result = model.fit()
+
+# AIC 값 확인
+aic = result.aic
