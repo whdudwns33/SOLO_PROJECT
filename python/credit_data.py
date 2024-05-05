36fa5d86a4a0c950bebbdf33f8d06a224b32a59a
jyj051811@gmail.com <jyj051811@gmail.com>
2024-05-05 17:40:25 +0900

    2024/05/ 데이터 정제 4차

--- python/credit_data.py
+++ python/credit_data.py
@@ -1,38 +1,45 @@
 import pandas as pd
 import statsmodels.api as sm
+import numpy as np
 
 # 데이터 파일 읽기
 df = pd.read_csv("credit_data.csv", low_memory=False, na_values='*')
+print("df:", df)
 
-# 원핫 인코딩 수행
+# 더미 변수 생성
 dummy = pd.get_dummies(df['LIV_ADD'], prefix='LIV_ADD', dtype=float)
-print(dummy.dtypes)
+print("dummy:", dummy)
 
-# # 두 데이터프레임을 열 방향으로 합치기
-combined_df = pd.concat([df, dummy], axis=1)
-non_numeric_columns = combined_df.select_dtypes(exclude=['number']).columns
+# 두 데이터프레임을 열 방향으로 합치기
+combined_df = df.join(dummy)
+print("combined_df:", combined_df)
 
-print("Non-numeric columns:", non_numeric_columns)
 
+number_columns = combined_df.select_dtypes(include=["number"])
+print("number_columns:", number_columns)
+
+# 결측치 제거
+final_df = number_columns.fillna(0)
+print("final_df:", final_df)
 
 # 종속 변수 선택
-y = combined_df["CB"]
+y = df["CB"].astype(float)
 
-# 독립 변수 선택 (Y를 제외한 모든 변수)
-X = combined_df.drop(columns=["CB", "SP", "LIV_ADD","RES_ADD","ADD_YN"])
+# 독립 변수 선택
+X = final_df.drop(columns=["CB", "SP", "식별구분", "결과값(연체회차)"]).astype(float)
+# x의 결측값, 무한값 확인
+# print("Nan : ",X.isnull().sum())
+# print(f"inf : {np.isinf(X).sum()}")
+
 
 # 모든 독립 변수에 상수항 추가
 X = sm.add_constant(X)
 
-print("Data types of y:", y.dtype)
+print("Data types of y:", y.dtypes)
 print("Data types of X:", X.dtypes)
-print("y values:", y.head())
-print("X values:", X.head())
-print("X type:", X.dtypes)
-print("X head:", X.head())
-
-
-
+# print("y values:", y.head())
+# print("X values:", X.head())
+# print("X head:", X.head())
 
 
 # 모델 생성
@@ -43,3 +50,4 @@ result = model.fit()
 
 # AIC 값 확인
 aic = result.aic
+print(f"aci : {aic}")
